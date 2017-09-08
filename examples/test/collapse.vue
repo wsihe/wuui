1637e349325b7ae7da881267184f6392289f588c
river <837606100@qq.com>
2017-09-08 23:15:29 +0800

    feat(collapse-item): 添加collapse-item折叠面板组件

--- examples/test/collapse.vue
+++ examples/test/collapse.vue
@@ -2,12 +2,36 @@
   .page
     h2 Collapse
     .demo-content
+      wu-collapse
+        wu-collapse-item(header="This is panel header 1")
+          p Collapse-Collapse-Collapse1
+        wu-collapse-item(header="This is panel header 2")
+          p Collapse-Collapse-Collapse2
+        wu-collapse-item(header="This is panel header 3")
+          p Collapse-Collapse-Collapse3
+    .demo-content
+      wu-collapse(bordered)
+        wu-collapse-item(header="This is panel header 1")
+          p Collapse-Collapse-Collapse1
+        wu-collapse-item(header="This is panel header 2")
+          p Collapse-Collapse-Collapse2
+        wu-collapse-item(header="This is panel header 3")
+          p Collapse-Collapse-Collapse3
+    .demo-content
+      wu-collapse(v-model="value", bordered, accordion)
+        wu-collapse-item(header="This is panel header 1", name="1")
+          p Collapse-Collapse-Collapse1
+        wu-collapse-item(header="This is panel header 2", name="2")
+          p Collapse-Collapse-Collapse2
+        wu-collapse-item(header="This is panel header 3", name="3")
+          p Collapse-Collapse-Collapse3
 </template>
 
 <script>
   export default {
     data () {
       return {
+        value: '1'
       }
     },
     created () {
