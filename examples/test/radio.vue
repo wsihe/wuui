a46caeea1a94ff28ee34d8ef924089ec63087a55
river <837606100@qq.com>
2017-09-12 09:42:53 +0800

    test(radio): radio api 测试

--- examples/test/radio.vue
+++ examples/test/radio.vue
@@ -2,29 +2,39 @@
   .page
     h2 Radio
     .demo-content
-      wu-radio 备选项
-      wu-radio 备选项
+      wu-radio(v-model="value1") 备选项
+      p {{value1}}
     .demo-content
-      wu-radio-group(size="large")
-        wu-radio-button 备选项
-        wu-radio-button 备选项
-        wu-radio-button 备选项
+      wu-radio-group(v-model="value2")
+        wu-radio(label="a") 男
+        wu-radio(label="b") 女
+        wu-radio(label="c") 其他
     .demo-content
       wu-radio-group
-        wu-radio-button 备选项
-        wu-radio-button 备选项
-        wu-radio-button 备选项
+        wu-radio(label="男")
+        wu-radio(label="女")
+        wu-radio(label="其他")
+    .demo-content
+      wu-radio-button(v-model="value3") 备选项
+    .demo-content
+      wu-radio-group(size="large")
+        wu-radio-button(label="a") 男
+        wu-radio-button(label="b") 女
+        wu-radio-button(label="c") 其他
     .demo-content
       wu-radio-group(size="small")
-        wu-radio-button 备选项
-        wu-radio-button 备选项
-        wu-radio-button 备选项
+        wu-radio-button(label="a") 男
+        wu-radio-button(label="b") 女
+        wu-radio-button(label="c") 其他
 </template>
 
 <script>
   export default {
     data () {
       return {
+        value1: false,
+        value2: 'a',
+        value3: false
       }
     },
     watch: {
