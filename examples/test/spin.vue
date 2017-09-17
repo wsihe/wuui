4e4f03e4bf274612c3131e701fc67e84a42e2168
river <837606100@qq.com>
2017-09-17 13:02:33 +0800

    test(spin):spin接口api测试

--- examples/test/spin.vue
+++ examples/test/spin.vue
@@ -3,15 +3,25 @@
     h2 Spin
     .demo-content
       wu-spin
+    .demo-content
+      .example
+        wu-spin
+    .demo-content
+      wu-spin
+        .example
+          p content of spin
+    .demo-content
+      wu-switch(@on-change="handle")
+      wu-spin(v-model="value")
+        .example
+          p content of spin
 </template>
 
 <script>
   export default {
     data () {
       return {
-        value1: false,
-        value2: 'a',
-        value3: false
+        value: false
       }
     },
     watch: {
@@ -19,15 +29,18 @@
     methods: {
       handle (val) {
         console.log(val)
-        console.log(this.value1)
-      },
-      handle2 (val) {
-        console.log(val)
-        console.log(this.value2)
+        this.value = val
       }
     }
   }
 </script>
 
 <style lang="stylus">
+  .example
+    text-align center
+    background rgba(0,0,0,0.05)
+    border-radius 4px;
+    margin-bottom 20px
+    padding 30px 50px
+    margin 20px 0
 </style>
