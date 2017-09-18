a9772fb822d92f055d3f6b9661847ae3a209702a
river <837606100@qq.com>
2017-09-18 09:51:27 +0800

    test(pagination):pagination接口api测试

--- examples/test/pagination.vue
+++ examples/test/pagination.vue
@@ -2,19 +2,24 @@
   .page
     h2 Pagination
     .demo-content
-      wu-pagination(:total="20")
+      wu-pagination(:total="50", :current.sync="current")
     .demo-content
       wu-pagination(:total="100")
+    .demo-content
+      wu-pagination(:total="100", size="small")
 </template>
 
 <script>
   export default {
     data () {
       return {
-        value1: ''
+        current: 1
       }
     },
     watch: {
+      current (page) {
+        console.log(page)
+      }
     },
     methods: {
     }
