cd77672c20afcbb12b7a0f1590d8c6a9b17bf6ed
river <837606100@qq.com>
2017-09-22 15:35:12 +0800

    feat(popover):添加popover气泡卡片

---
+++ examples/test/popover.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  .page
+    h2 Popover
+    .demo-content
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        input: '',
+        input2: 'textarea',
+        input3: 'autosize'
+      }
+    },
+    watch: {
+      input (newVal, oldVal) {
+        console.log(newVal)
+      }
+    },
+    methods: {
+    }
+  }
+</script>x
---
+++ src/packages/popover/src/popover.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div
+    slot
+</template>
+
+<script>
+  //  const prefixCls = 'wu-menu-item-group'
+  export default {
+    name: 'WuPopover',
+
+    componentName: 'WuPopover',
+
+    props: {
+    },
+
+    data () {
+      return {
+        isGroup: true
+      }
+    }
+  }
+</script>
