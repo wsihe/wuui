06d6ce03a885baf42bd4600c3367951cebf60286
river <837606100@qq.com>
2017-09-25 13:30:14 +0800

    feat(popover):添加popover气泡卡片

--- examples/test/popover.vue
+++ examples/test/popover.vue
@@ -2,20 +2,23 @@
   .page
     h2 Popover
     .demo-content
+      wu-popover(title="title" content="popover text")
+        wu-button Hover me
+    .demo-content
+      wu-popover(title="title" placement="top")
+        span(slot="content") popover text
+        wu-button Hover me
+    .demo-content
+      wu-popover(title="title" placement="top" trigger="click")
+        span(slot="content") popover text
+        wu-button Click me
 </template>
 
 <script>
   export default {
     data () {
       return {
-        input: '',
-        input2: 'textarea',
-        input3: 'autosize'
-      }
-    },
-    watch: {
-      input (newVal, oldVal) {
-        console.log(newVal)
+        input: ''
       }
     },
     methods: {
--- src/packages/popover/src/popover.vue
+++ src/packages/popover/src/popover.vue
@@ -1,22 +1,108 @@
 <template lang="pug">
-  div
+  .wu-popover-wrap(v-clickoutside="hide")
     slot
+    .wu-popover(:class="tooltipCls", ref="popper", v-show="showPopper")
+      .wu-popover-content
+        .wu-popover-arrow
+        .wu-popover-inner
+          div
+            .wu-popover-title {{title}}
+            .wu-popover-inner-content
+              slot(name="content") {{content}}
 </template>
 
 <script>
-  //  const prefixCls = 'wu-menu-item-group'
+  import clickoutside from 'wuui/directives/clickoutside'
+  import Popper from 'wuui/mixins/popper'
+
+  const prefixCls = 'wu-popover'
   export default {
     name: 'WuPopover',
 
     componentName: 'WuPopover',
 
+    mixins: [Popper],
+
+    directives: {clickoutside},
+
     props: {
+      title: String,
+      content: String,
+      trigger: {
+        type: String,
+        default: 'hover'
+      },
+      enterable: {
+        type: Boolean,
+        default: true
+      },
+      openDelay: {
+        type: Number,
+        default: 0
+      },
+      popperOptions: {
+        default () {
+          return {
+            boundariesPadding: 10,
+            gpuAcceleration: false
+          }
+        }
+      }
     },
 
     data () {
       return {
-        isGroup: true
+        timeout: null
       }
+    },
+
+    computed: {
+      tooltipCls () {
+        return {
+          [`${prefixCls}-placement-${this.placement}`]: !!this.placement
+        }
+      }
+    },
+
+    methods: {
+      show () {
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(() => {
+          this.showPopper = true
+        })
+      },
+      hide () {
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(() => {
+          this.showPopper = false
+        })
+      },
+      toggleClick () {
+        if (this.disabled) return
+        this.showPopper = !this.showPopper
+      },
+      initEvent () {
+        let {enterable, trigger, show, hide, toggleClick} = this
+        let triggerElm = this.$el
+
+        if (trigger === 'hover') {
+          triggerElm.addEventListener('mouseenter', show)
+          triggerElm.addEventListener('mouseleave', hide)
+
+          if (enterable) {
+            let popperElm = this.$refs.popper
+            popperElm.addEventListener('mouseenter', show)
+            popperElm.addEventListener('mouseleave', hide)
+          }
+        } else if (trigger === 'click') {
+          triggerElm.addEventListener('click', toggleClick)
+        }
+      }
+    },
+
+    mounted () {
+      this.referenceElm = this.$el
+      this.initEvent()
     }
   }
 </script>
