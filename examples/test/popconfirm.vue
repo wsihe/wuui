bd56acd0979d437f180d83da327725182dae59ac
river <837606100@qq.com>
2017-09-29 14:40:24 +0800

    feat(popconfirm):添加popconfirm测试

--- examples/test/popconfirm.vue
+++ examples/test/popconfirm.vue
@@ -2,14 +2,14 @@
   .page
     h2 Popconfirm
     .demo-content
-      wu-popconfirm(title="确定删除 ？")
+      wu-popconfirm(title="您是否确定要删除此项 ？")
         a 删除
     .demo-content
-      wu-popconfirm(title="确定删除 ？", @on-confirm="handleConfirm", @on-cancel="handleCancel")
+      wu-popconfirm(title="您是否确定要删除此项 ？", @on-confirm="handleConfirm", @on-cancel="handleCancel")
         a 删除
     .demo-content
       wu-switch(@on-change="handle", :style="{marginBottom:'20px',marginRight:'40px'}")
-      wu-popconfirm(title="确定删除 ？", :disabled="disabled")
+      wu-popconfirm(title="您是否确定要删除此项 ？", :disabled="disabled")
         a 删除
 </template>
 
