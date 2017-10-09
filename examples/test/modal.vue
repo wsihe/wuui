ac30db4c045eb71b5ed83ae225669a07fd02b983
river <837606100@qq.com>
2017-10-09 14:25:06 +0800

    feat(modal):添加对话框

--- examples/test/modal.vue
+++ examples/test/modal.vue
@@ -2,18 +2,27 @@
   .page
     h2 Modal
     .demo-content
+      wu-button(type="primary", @click="modalVisible = true") 打开对话框
+      wu-modal(title="提示", v-model="modalVisible", @on-ok="handleOk", @on-cancel="handleCancel")
+        p 内容...
+        p 内容...
+        p 内容...
 </template>
 
 <script>
   export default {
     data () {
       return {
-        value: false
+        modalVisible: false
       }
     },
-    watch: {
-    },
     methods: {
+      handleOk () {
+        this.$message('确定')
+      },
+      handleCancel () {
+        this.$message('取消')
+      }
     }
   }
 </script>
--- src/packages/modal/src/modal.vue
+++ src/packages/modal/src/modal.vue
@@ -1,27 +1,99 @@
 <template lang="pug">
-  div
-    slot
+  div(v-show="visible")
+    .wu-modal-mask
+    .wu-modal-wrap(@click="handleWrapClick")
+      .wu-modal(:style="modalStyle")
+        .wu-modal-content
+          button.wu-modal-close(@click="visible = false")
+            span.wu-modal-close-x
+          .wu-modal-header
+            .wu-modal-title {{title}}
+          .wu-modal-body
+            slot
+          .wu-modal-footer
+            slot(name="footer")
+              wu-button(size="large", @click="handleCancelClick") {{cancelText}}
+              wu-button(type="primary", size="large", @click="handleOkClick") {{okText}}
 </template>
 
 <script>
+  import WuButton from '../../button/src/button'
 //  const prefixCls = 'wu-modal'
   export default {
+    components: {WuButton},
     name: 'WuModal',
 
     componentName: 'WuModal',
 
     props: {
+      value: {
+        type: Boolean,
+        default: false
+      },
+      title: String,
+      cancelText: {
+        type: String,
+        default: '取消'
+      },
+      okText: {
+        type: String,
+        default: '确定'
+      },
+      width: {
+        type: [String, Number],
+        default: 520
+      },
+      maskClosable: {
+        type: Boolean,
+        default: true
+      }
     },
 
     data () {
       return {
+        visible: this.value
+      }
+    },
+
+    watch: {
+      value (val) {
+        this.visible = val
+      },
+      visible (val) {
+        this.$emit('input', val)
       }
     },
 
     computed: {
+      modalStyle () {
+        let width = parseInt(this.width)
+        return {
+          width: `${width}px`
+        }
+      }
     },
 
     created () {
+    },
+
+    methods: {
+      handleWrapClick (e) {
+        const className = e.target.getAttribute('class')
+        if (className && className.indexOf('wrap') > -1) {
+          this.closeModal()
+        }
+      },
+      handleOkClick () {
+        this.visible = false
+        this.$emit('on-ok')
+      },
+      handleCancelClick () {
+        this.visible = false
+        this.$emit('on-cancel')
+      },
+      closeModal () {
+        if (this.maskClosable) this.visible = false
+      }
     }
   }
 </script>
