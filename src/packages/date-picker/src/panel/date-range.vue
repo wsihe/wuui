0e35bce418e09a0a77cdfeaac2e433f7238a2dd4
river <837606100@qq.com>
2017-10-11 16:59:14 +0800

    feat(date-picker):添加日期选择框

---
+++ src/packages/date-picker/src/panel/date-range.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div
+    slot
+</template>
+
+<script>
+  //  const prefixCls = 'wu-modal'
+  export default {
+    name: '',
+
+    componentName: '',
+
+    props: {
+    },
+
+    data () {
+      return {
+      }
+    },
+
+    computed: {
+    },
+
+    created () {
+    }
+  }
+</script>
---
+++ src/packages/date-picker/src/picker/date-picker.js
@@ -0,0 +1,20 @@
+// import DatePanel from '../panel/date'
+// import DateRangePanel from '../panel/date-range'
+
+export default {
+
+  name: 'WuDatePicker',
+
+  props: {
+    type: {
+      type: String,
+      default: 'date'
+    }
+  },
+
+  watch: {
+  },
+
+  created () {
+  }
+}
