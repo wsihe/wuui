685e805a74a0200f039618dc92bfe0a05741c458
river <837606100@qq.com>
2017-10-30 14:19:22 +0800

    feat(loading-bar):添加时间伪加载条

---
+++ src/packages/loading-bar/src/loading.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .wu-loding-bar
+</template>
+
+<script>
+//  const prefixCls = 'wu-loading-bar'
+  export default {
+    name: 'WuLoadingBar',
+
+    componentName: 'WuLoadingBar',
+
+    props: {
+    },
+
+    data () {
+      return {
+      }
+    },
+
+    computed: {
+    },
+
+    methods: {
+    },
+
+    created () {
+    }
+  }
+</script>
